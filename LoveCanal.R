34770ca2d8f75c9505885c59be09b0bc6716ac7e
Adam M. Wilson <adammichaelwilson@gmail.com>
2016-11-22 13:48:01 -0500

    Love canal map

--- LoveCanal.R
+++ LoveCanal.R
@@ -1,10 +1,19 @@
 ## Love Canal
 
 library(leaflet)
+library(ggplot2)
+library(ggmap)
+library(dplyr)
+library(htmlwidgets)
+
+lc=geocode("Love Canal, NY")
+ub=geocode("University at Buffalo North Campus, Amherst, NY")
 
 m <- leaflet() %>%
   addTiles() %>%  # Add default OpenStreetMap map tiles
-  addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
+  addMarkers(lng=lc$lon, lat=lc$lat, popup="The Love Canal") %>%
+  addMarkers(lng=ub$lon, lat=ub$lat, popup="University at Buffalo")%>%
+  setView(lng=ub$lon, lat=ub$lat+.1,10)
 m  # Print the map
 
-
+saveWidget(m, file="LoveCanal.html")
