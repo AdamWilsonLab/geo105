bbee4a04b6161be4d26b62200e61e0d347ed3738
Adam M. Wilson <adammichaelwilson@gmail.com>
2020-01-15 09:26:18 -0500

    update to include cumulative snow data

--- Elma_snow_data.R
+++ Elma_snow_data.R
@@ -2,6 +2,7 @@
 
 library(rgdal)
 library(sf)
+library(scales)
 
 # Anomalies
 library(rnoaa)
@@ -11,9 +12,10 @@ library(ggpmisc)
 library(zoo)
 library(lubridate)
 
-stations=ghcnd_stations()
-
 d=as.Date("2018-07-01")
+
+if(F){
+stations=ghcnd_stations()
 
 stations%>%filter(
   grepl("ELMA CENTER",name),
@@ -22,9 +24,24 @@ stations%>%filter(
   last_year>=2017,
 )
 
-data=meteo_tidy_ghcnd("US1NYER0075",var=c("SNOW","SNWD"),date_min = as.Date("1940-01-01"))
+stations%>%filter(
+  grepl("BUFFALO",name),
+  grepl("NY",state),
+  element=="SNOW",
+  last_year>=2017,
+)
 
-data_2=data%>%mutate(
+}
+
+
+data=meteo_tidy_ghcnd(#"US1NYER0075", #elma
+                      "USW00014733", #Buffalo
+                      var=c("SNOW","SNWD"),
+                      date_min = as.Date("1900-01-01")
+                      )
+
+data_2=data%>%
+  mutate(
   snow=ifelse(is.na(snow),0,snow*.039),
   snwd=ifelse(is.na(snwd),0,snwd*.039),
   day=as.numeric(format(date,"%d")),
@@ -34,7 +51,10 @@ data_2=data%>%mutate(
   dyear=year+(month/12),
   winter=ifelse(month<6,year-1,year),
   fyear=ymd(paste(ifelse(month<6,2018,2017),month,day)),
-  season=between(fyear,ymd("20180105"),ymd("20180305")))%>%
+  season=between(fyear,ymd("20180105"),ymd("20180305")),
+  decade=floor(winter/10)*10,
+  era=cut(winter,breaks = c(1900,2000,2020)))%>%
+#  filter(winter>2009) %>% 
   group_by(winter)
 
 firstday<-
@@ -60,8 +80,74 @@ data_winter
 ggplot(data_2,aes(x=fyear))+
   facet_wrap(~winter,ncol=1)+
   xlim(ymd(c("20171101","20180501")))+
-  geom_rect(aes(xmin=ymd("20170105"),xmax=ymd("20180305"),ymin=0,ymax=32))+
+  geom_rect(aes(xmin=ymd("20170105"),
+                xmax=ymd("20180305"),ymin=0,ymax=32))+
   geom_area(aes(y=snwd),fill=grey(0.6))+
   geom_line(aes(y=snow),col="red")
 
-    
+### Cumulative Snowfall
+sum_snow <- data_2 %>% 
+  group_by(winter) %>% 
+  arrange(dyear) %>% 
+  mutate(snowfall=cumsum(snow),
+         maxsnow=max(snowfall))
+
+sum_snow %>% 
+  ggplot(aes(x=fyear,y=snowfall,color=winter,
+             group=as.factor(winter)))+
+  geom_line()+
+  geom_line(data=filter(sum_snow,winter==2019),col="black",size=2)+
+  geom_text(aes(label=winter,x=ymd("20170401"),y=maxsnow))+
+  scale_color_viridis_c(name="Year")+
+    scale_x_date(name = "Date",
+               limits = ymd(c("20171015","20180501")),
+               breaks='1 month',
+               labels = date_format("%b"))+
+    geom_smooth(aes(group=1),col="red")+
+  ylab("Cumulative Snowfall (cm)")
+
+
+### Mean Snowdepth
+mean_snowd <- data_2 %>% 
+  group_by(winter) %>% 
+  arrange(dyear) %>% 
+  mutate(snowfall=rollmean(snwd,k = 5,fill = NA))
+
+mean_snowd %>% 
+  ggplot(aes(x=fyear,y=snowfall,color=winter,
+             group=as.factor(winter)))+
+  geom_line()+
+  geom_line(data=filter(mean_snowd,winter==2019),
+            col="black",size=2)+
+  scale_color_viridis_c(name="Year")+
+  xlim(ymd(c("20171015","20180501")))+
+  geom_smooth(aes(color=decade,group=decade),span=.1,col="red")+
+  ylab("Mean Snow Depth (cm)")
+
+
+### Skiable days
+### 
+ski_depth=4
+
+data_2 %>% 
+  group_by(winter, era) %>% 
+  arrange(dyear) %>% 
+  mutate(ski_day=snwd>ski_depth,
+         ski_day_smooth=rollmean(ski_day,k = 3,fill = NA)) %>% 
+  group_by(fyear, era) %>% 
+  summarize(ski_day=mean(ski_day_smooth)) %>% 
+  ggplot(aes(x=fyear,y=ski_day*100,color=era, group=era))+
+  geom_point()+
+  geom_smooth(span=.5)+
+  ylab("Proportion Skiable Days (%)")+
+  scale_x_date(name = "Date",
+               limits = ymd(c("20171015","20180501")),
+               breaks='1 month',
+               labels = date_format("%b"))+
+  ggtitle("Proportion Skiable Days in Elma, NY",
+          subtitle = paste("% Days with Snowdepth >",
+                           ski_depth,"cm over ",
+                           paste(range(data_2$year),
+                                 collapse="-")))
+  
+
